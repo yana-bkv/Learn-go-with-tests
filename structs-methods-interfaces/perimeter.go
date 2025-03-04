9af9a1fa73be1ed00607e22423de8b17e795d5d4
jaboikova <yanis.twink@gmail.com>
2025-03-04 21:04:05 +0200

    checkArea

---
+++ structs-methods-interfaces/perimeter.go
@@ -0,0 +1,28 @@
+package main
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return (rectangle.Width + rectangle.Height) * 2
+}
+
+func (rectangle Rectangle) Area() float64 {
+	return rectangle.Width * rectangle.Height
+}
+
+func (circle Circle) Area() float64 {
+	return circle.Radius * circle.Radius * math.Pi
+}
